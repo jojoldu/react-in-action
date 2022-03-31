ac7512862a96335012ee2cdfee9f323e68c8ce28
jojoldu <jojoldu@gmail.com>
2022-03-31 09:52:20 +0900

    theme 추가

---
+++ settings/constant.js
@@ -0,0 +1,8 @@
+// General Page Section
+export const HOME_PAGE = '/';
+export const AGENTS_PAGE = '/agents';
+
+// Listing Single Page Section
+export const LISTING_POSTS_PAGE = '/listing';
+export const SINGLE_POST_PAGE = '/post';
+
